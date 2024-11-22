992da9bfc3b9a7feb04b606e9d6dd67e10fe1252
Pierre Janineh <janinehpierre@gmail.com>
2024-11-22 11:06:16 +0200

    Initial Commit

---
+++ CocaCola/CocaColaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocaColaApp.swift
+//  CocaCola
+//
+//  Created by Pierre Janineh on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CocaColaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
